c7e6b0d9e95efbcf10c7215f35fed3f08d244ff9
Maxim N <mnosov622@gmail.com>
2023-01-27 12:31:00 -0500

    show message if there are no bought courses

--- frontend/src/pages/LearnerCourses/LearnerCourses.jsx
+++ frontend/src/pages/LearnerCourses/LearnerCourses.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react";
 import CourseCard from "../../components/CourseCards/CourseCard";
+import empty from "../../assets/no-courses.gif";
+import { Link } from "react-router-dom";
+
 const LearnerCourses = () => {
   useEffect(() => {
     console.log("Component init");
@@ -13,14 +16,30 @@ const LearnerCourses = () => {
       <div className="bg-light shadow text-center p-2 fs-2 mb-4">
         <p>My Courses</p>
       </div>
-      <CourseCard
-        cardSmall
-        name={item?.name}
-        price={item?.price}
-        image={item?.courseImage}
-        teacherName={item?.instructor}
-        id={item?.id}
-      />
+      {!item ? (
+        <p>
+          <div className="text-center mb-5">
+            <p className="fs-3">There are no courses yet...</p>
+            <img src={empty} alt="No courses" className="ml-auto mb-5" />
+            <div className="wrapper">
+              <Link to="/all-courses">
+                <button className="btn btn-primary btn-lg">
+                  Explore all courses
+                </button>
+              </Link>
+            </div>
+          </div>{" "}
+        </p>
+      ) : (
+        <CourseCard
+          cardSmall
+          name={item?.name}
+          price={item?.price}
+          image={item?.courseImage}
+          teacherName={item?.instructor}
+          id={item?.id}
+        />
+      )}
     </>
   );
 };
