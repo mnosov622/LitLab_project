946514fa166389c255116e0e572e0820e5b06f45
aneesm8 <anees.mohammed2002@gmail.com>
2023-02-02 19:30:24 -0500

    Updated Analytics

--- frontend/src/pages/CreatorDashboard/Analytics/CreatorAnalytics.jsx
+++ frontend/src/pages/CreatorDashboard/Analytics/CreatorAnalytics.jsx
@@ -1,46 +1,36 @@
-import React, { useState } from "react";
-import { Bar } from "react-chartjs-2";
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+const data = [
+  { month: 'Jan', enrolled: 50 },
+  { month: 'Feb', enrolled: 60 },
+  { month: 'Mar', enrolled: 55 },
+  { month: 'Apr', enrolled: 75 },
+  { month: 'May', enrolled: 90 },
+  { month: 'Jun', enrolled: 85 },
+  { month: 'Jul', enrolled: 80 },
+  { month: 'Aug', enrolled: 65 },
+  { month: 'Sep', enrolled: 70 },
+  { month: 'Oct', enrolled: 75 },
+  { month: 'Nov', enrolled: 60 },
+  { month: 'Dec', enrolled: 55 }
+];
 
 const CreatorAnalytics = () => {
-  const [data, setData] = useState({
-    labels: [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-    ],
-    datasets: [
-      {
-        label: "Enrollments",
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        borderColor: "rgba(255, 99, 132, 1)",
-        borderWidth: 1,
-        hoverBackgroundColor: "rgba(255, 99, 132, 0.4)",
-        hoverBorderColor: "rgba(255, 99, 132, 1)",
-        data: [65, 59, 80, 81, 56, 55, 40, 45, 67, 23, 12, 56]
-      }
-    ]
-  });
-
   return (
-    <div>
-      <Bar
-        data={data}
-        width={100}
-        height={50}
-        options={{
-          maintainAspectRatio: false
-        }}
-      />
-    </div>
+    <BarChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <CartesianGrid strokeDasharray='3 3' />
+      <XAxis dataKey='month' />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey='enrolled' fill='#8884d8' />
+    </BarChart>
   );
 };
 
