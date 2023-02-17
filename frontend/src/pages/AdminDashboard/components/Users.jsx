71eebd28c80c6496edc15b0359625a03f9b54a4c
Maxim N <mnosov622@gmail.com>
2023-02-16 22:15:24 -0500

    admin dashboard update

--- frontend/src/pages/AdminDashboard/components/Users.jsx
+++ frontend/src/pages/AdminDashboard/components/Users.jsx
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import CourseCard from "../../../components/CourseCards/CourseCard";
 
 const Users = () => {
   const [usersData, setUsersData] = useState([]);
+  const [coursesData, setCoursesData] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:8000/users")
       .then((res) => res.json())
       .then((data) => setUsersData(data));
+
+    fetch("http://localhost:8000/courses")
+      .then((res) => res.json())
+      .then((data) => setCoursesData(data));
   }, []);
 
   return (
     <div>
-      <h3 className="text-center mb-4">All Users</h3>
+      <h2 className="text-center mb-4">All Users</h2>
       <table className="table  table-light fs-5">
         <thead className="border">
           <tr className="text-dark">
@@ -35,6 +42,12 @@ const Users = () => {
           ))}
         </tbody>
       </table>
+
+      <div className="container mt-5 text-center">
+        <Link to="/all-courses" className="btn btn-primary btn-lg">
+          See all courses
+        </Link>
+      </div>
     </div>
   );
 };
