eee97c44762184abd61d1ca91aaa3e37b0541ee6
puja1102 <pgkakani@myseneca.ca>
2023-02-17 11:13:23 -0500

    search page

--- frontend/src/pages/Search/SearchBar.jsx
+++ frontend/src/pages/Search/SearchBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import courses from "../../fakeData/courses.json";
 import { getOptions } from "../../utils/getOptions";
 import CourseDescription from '../CourseDescription/CourseDescription';
-//import SearchResults from './SearchResults';
+import SearchResults from './SearchResults';
 import { useNavigate } from "react-router-dom";
 
 const SearchBar = (props) => {
@@ -38,12 +38,12 @@ const SearchBar = (props) => {
 
   return (
     <div className="col-md-3">
-              <form action="search-result"  onSubmit={handleSubmit}>
+              <form action=""  onSubmit={handleSubmit}>
                 <div className="input-group search">
                   <input
                     type="text"
-                    id='input'
-                    value={searchValue}
+                    //id='input'
+                    //value={searchValue}
                     className="form-control w-100 mt-4"
                     placeholder="Search for your favourite courses"
                     aria-label="Search for your favourite courses"
@@ -81,11 +81,12 @@ const SearchBar = (props) => {
                   </ul>
                 )}
               </form>
-              </div>
-
               
+    </div>    
    
   );
 }
+
+
 
 export default SearchBar;
--- frontend/src/pages/Search/SearchResults.jsx
+++ frontend/src/pages/Search/SearchResults.jsx
@@ -3,18 +3,19 @@ import { useLocation } from 'react-router-dom';
 import CourseCard from '../../components/CourseCards/CourseCard'; 
 import searchValue from './SearchBar.jsx';
 
-function SearchResults() {
+function SearchResults(props) {
+  const { searchQuery } = props;
   //const location = useLocation();
   //const [searchValue, setSearchValue] = useState("");
-  //const query = new URLSearchParams(location.search).get("/search-result");
+  //const searchValue = new URLSearchParams(location.search).get("/search-result");
   //const CourseCards = location.state.CourseCards;
 
   return (
     <>
-      <h1>Results for {searchValue}</h1>
-      <div>
-    <CourseCard/> 
-      </div>
+    
+      <h1>Results for {searchQuery}</h1>
+      <CourseCard />
+      
     </>
   );
 }
