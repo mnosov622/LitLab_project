125c0b20961b17a996f940c880c136b7a4032d77
Maxim Nosov <maximnosov@Maxims-MacBook-Pro.local>
2023-02-19 17:00:53 -0500

    display amount of enrollments and courses for content creator analytics

--- frontend/src/pages/CreatorDashboard/Analytics/CreatorAnalytics.jsx
+++ frontend/src/pages/CreatorDashboard/Analytics/CreatorAnalytics.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import jwtDecode from "jwt-decode";
+import React, { useEffect, useState } from "react";
 import {
   BarChart,
   Bar,
@@ -77,10 +78,39 @@ const EarningsChart = () => {
 };
 
 const Charts = () => {
+  const token = localStorage.getItem("token");
+  const decoded = jwtDecode(token);
+  const userId = decoded.id;
+
+  const [courses, setCourses] = useState([]);
+  const [coursesLength, setCoursesLength] = useState([]);
+  const [enrollmentsAmount, setEnrollmentsAmount] = useState(0);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/users/${userId}`)
+    .then(res => res.json())
+    .then(data => {setCourses(data.courses); setCoursesLength(data.courses.length); 
+      
+      console.log(data.courses.map(course => course.enrollments));
+      const enrollemnts = data.courses.map(course => course.enrollments);
+      const enrollmentsAmount = enrollemnts.reduce((acc, val) => acc + val, 0);
+      console.log("Endollments sum", enrollmentsAmount);
+      setEnrollmentsAmount(enrollmentsAmount);
+    })
+  }, []);
+
   return (
     <>
       <div className="bg-light shadow text-center p-2 fs-2 mb-5">
         <p>My Analytics</p>
+      </div>
+      <div className="mb-5 row text-center">
+        <div className="col-md-6 ml-auto">
+          <h3>Total courses: {coursesLength}</h3>
+        </div>
+        <div className="col-md-6">
+          <h3>Total enrollments: {enrollmentsAmount}</h3>
+        </div>
       </div>
       <div className="charts-container mb-5">
         <EnrollmentChart />
