4f4a96c11d79fa795eb89fc74fab975e28e73da5
Maxim N <mnosov622@gmail.com>
2023-03-08 22:12:22 -0500

    bug fix

--- frontend/src/pages/FooterContent/ThankYou.jsx
+++ frontend/src/pages/FooterContent/ThankYou.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import accepted from '../../assets/accepted.png';
+import React from "react";
+import accepted from "../../assets/accepted.png";
 
 const ThankYou = () => {
-    return (
-        <div className='text-center mt-5 mb-5 pb-5 fs-2'>
-            <img src={accepted} alt="accepted" width={"5%"}/>
-            We recieved your message <br/>and will answer you within 2 business days</div>
-    )
-}
+  return (
+    <div className="text-center mt-5 mb-5 pb-5 fs-2">
+      <img src={accepted} alt="accepted" width={"5%"} />
+      We recieved your message <br />
+      and will answer you within 2 business days
+    </div>
+  );
+};
 
 export default ThankYou;
