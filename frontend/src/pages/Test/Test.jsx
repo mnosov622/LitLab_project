4568dc1a838ef01094508b2db1a835a120962145
Maxim N <mnosov622@gmail.com>
2023-03-30 16:27:20 -0400

    test view update

--- frontend/src/pages/Test/Test.jsx
+++ frontend/src/pages/Test/Test.jsx
@@ -51,7 +51,7 @@ const Question = ({
       className="flex items-center"
     >
       <div style={{ textAlign: "center" }}>
-        <h2>{question}</h2>
+        {/* <h2>{question}</h2>
         {options.map((option, index) => (
           <div key={index} style={{ margin: "auto", width: "100%" }}>
             <input
@@ -67,7 +67,7 @@ const Question = ({
               {option}
             </label>
           </div>
-        ))}
+        ))} */}
         {selectedOption && (
           <p>
             {selectedOption === correctOption ? (
@@ -137,6 +137,23 @@ const Test = () => {
   const { id } = useParams();
   const userEmail = decoded.email;
 
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionClick = (selectedOption, correctOption) => {
+    setSelectedOption(selectedOption);
+    console.log("selected", selectedOption, correctOption);
+    if (selectedOption === correctOption) {
+      console.log("correct");
+      handleCorrectAnswer();
+    } else {
+      console.log("bad");
+    }
+  };
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   // const questions = [
   //   {
   //     question: 'What is the capital of France?',
@@ -155,6 +172,7 @@ const Test = () => {
   );
 
   const handleNext = () => {
+    setSelectedOption(null);
     setCurrentIndex(currentIndex + 1);
   };
 
@@ -253,10 +271,75 @@ const Test = () => {
       ) : (
         <>
           {question.length > 0 ? (
-            <p className="fs-1 text-center mb-5 d-flex justify-content-center align-items-center text-primary">
-              Complete a test to get certificate &nbsp;&nbsp;
-              <i className="bi bi-patch-check"></i>
-            </p>
+            <>
+              <p className="fs-1 text-center mb-5 d-flex justify-content-center align-items-center text-primary">
+                Complete a test to get certificate &nbsp;&nbsp;
+                <i className="bi bi-patch-check"></i>
+              </p>
+              <div
+                className="form-item question"
+                key={question[currentIndex].question}
+              >
+                <h2 className="mb-3">{question[currentIndex].question}</h2>
+                {question[currentIndex].options.map((option) => (
+                  <>
+                    <div className="mb-2 d-flex align-items-center">
+                      <input
+                        type="radio"
+                        name={option}
+                        value={option}
+                        id={option}
+                        checked={selectedOption === option}
+                        onChange={() =>
+                          handleOptionClick(
+                            option,
+                            question[currentIndex].correctAnswer
+                          )
+                        }
+                        style={{ width: "1.5em", transform: "scale(1.5)" }}
+                      />
+                      <span>{option}</span>
+                    </div>
+                  </>
+                ))}
+                {selectedOption && (
+                  <p>
+                    {selectedOption === question[currentIndex].correctAnswer ? (
+                      <>
+                        <p className="text-success">Correct!</p>
+                      </>
+                    ) : (
+                      <>
+                        <p className="text-danger">Incorrect, try again.</p>
+                      </>
+                    )}
+                  </p>
+                )}
+
+                {question[currentIndex].input}
+              </div>
+
+              <div className="text-center mt-5 mb-5">
+                <button
+                  className="btn btn-secondary"
+                  style={{ marginRight: "3%" }}
+                  onClick={handlePrev}
+                  disabled={currentIndex === 0}
+                >
+                  Previous
+                </button>
+                <button
+                  className="btn btn-primary"
+                  onClick={handleNext}
+                  disabled={
+                    currentIndex === question.length - 1 ||
+                    selectedOption !== question[currentIndex].correctAnswer
+                  }
+                >
+                  Next
+                </button>
+              </div>
+            </>
           ) : (
             <>
               <p className="fs-1 text-center mb-5 d-flex justify-content-center align-items-center text-primary">
@@ -273,18 +356,6 @@ const Test = () => {
               </div>
             </>
           )}
-
-          {question.map((question, index) => (
-            <>
-              <Question
-                key={index}
-                question={question.question}
-                options={question.options}
-                correctOption={question.correctAnswer}
-                handleCorrectAnswer={handleCorrectAnswer}
-              />
-            </>
-          ))}
 
           {correctAnswers === totalQuestions && (
             <>
@@ -305,208 +376,7 @@ const Test = () => {
         </>
       )}
     </div>
-    // <div className="test-container">
-    //   <Question
-    //     question={question[currentIndex].question}
-    //     options={question[currentIndex].options}
-
-    //   />
-    // </div>
   );
 };
-
-// const Question = ({
-//   question,
-//   options,
-//   correctOption,
-//   handleCorrectAnswer,
-// }) => {
-//   const [selectedOption, setSelectedOption] = useState(null);
-
-// const handleOptionClick = (selectedOption) => {
-//   setSelectedOption(selectedOption);
-//   if (selectedOption === correctOption) {
-//     console.log("correct");
-//     handleCorrectAnswer();
-//   } else {
-//     console.log("bad");
-//   }
-// };
-
-//   return (
-//     <div className="mt-5 pb-3">
-//       <h3 className="border-bottom">{question}</h3>
-//       {options.map((option, index) => (
-//         <div key={index}>
-//           <input
-//             type="radio"
-//             name={option}
-//             id={option}
-//             value={option}
-//             checked={selectedOption === option}
-//             onChange={() => handleOptionClick(option)}
-//           />
-//           <label htmlFor={option}>{option}</label>
-//         </div>
-//       ))}
-//       {selectedOption && (
-//         <p>
-//           {selectedOption === correctOption ? (
-//             <>
-//               <p className="text-success">Correct!</p>
-//             </>
-//           ) : (
-//             <>
-//               <p className="text-danger">Incorrect, try again.</p>
-//             </>
-//           )}
-//         </p>
-//       )}
-//     </div>
-//   );
-// };
-
-// const Test = () => {
-// const [question, setQuestions] = useState([]);
-// const [loading, setLoading] = useState(false);
-// const [correctAnswers, setCorrectAnswers] = useState(null);
-// const totalQuestions = question.length;
-// const [running, setIsRunning] = useState(true);
-// const token = localStorage.getItem("token");
-// const decoded = jwtDecode(token);
-// const { id } = useParams();
-// const userEmail = decoded.email;
-
-// const handleCorrectAnswer = () => {
-//   setCorrectAnswers(correctAnswers + 1);
-// };
-// useEffect(() => {
-//   //If users answers all questions correctly, make a PUT request to set the value of Completed to true
-//   if (correctAnswers === totalQuestions) {
-//     const fetchData = async () => {
-//       const response = await fetch(
-//         `https://backend-litlab.herokuapp.com/users/${userEmail}/courses/${id}`,
-//         {
-//           method: "PUT",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify({
-//             userEmail: userEmail,
-//             courseId: id,
-//           }),
-//         }
-//       );
-
-//       console.log(response);
-//       if (!response.ok) {
-//         console.log("error");
-//         throw new Error(response.statusText);
-//       }
-//     };
-//     fetchData();
-//   }
-// }, [totalQuestions, correctAnswers]);
-
-// useEffect(() => {
-//   if (question.length === 0) {
-//     fetch(`https://backend-litlab.herokuapp.com/users/${userEmail}/courses/${id}`, {
-//       method: "PUT",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         userEmail: userEmail,
-//         courseId: id,
-//       }),
-//     });
-//   }
-// }, []);
-// setTimeout(() => {
-//   setIsRunning(false);
-// }, 3000);
-
-// useEffect(() => {
-//   setLoading(true);
-//   fetch(`https://backend-litlab.herokuapp.com/courses/${Number(id)}`)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       setQuestions(data.course.test);
-//       setLoading(false);
-//       console.log("course", data);
-//     });
-// }, []);
-
-//   return (
-// <div className={loading && "bottom"}>
-//   {loading ? (
-//     <Oval
-//       height={80}
-//       width={80}
-//       color="#0d6efd"
-//       wrapperStyle={{ position: "absolute", left: "50%", top: "40%" }}
-//       wrapperClass=""
-//       visible={true}
-//       ariaLabel="oval-loading"
-//       secondaryColor="#0d6efd"
-//       strokeWidth={2}
-//       strokeWidthSecondary={2}
-//     />
-//   ) : (
-//     <>
-//       {question.length > 0 ? (
-//         <p className="fs-1 text-center mb-5 d-flex justify-content-center align-items-center text-primary">
-//           Complete a test to get certificate &nbsp;&nbsp;
-//           <i className="bi bi-patch-check"></i>
-//         </p>
-//       ) : (
-//         <>
-//           <p className="fs-1 text-center mb-5 d-flex justify-content-center align-items-center text-primary">
-//             Content Creator didn't create a test
-//           </p>
-//           <div className="text-center">
-//             <Link
-//               to={`/certificate/${id}`}
-//               role="button"
-//               className="btn btn-primary btn-lg mb-3"
-//             >
-//               Get Certificate
-//             </Link>
-//           </div>
-//         </>
-//       )}
-
-//       {question.map((question, index) => (
-//         <>
-//           <Question
-//             key={index}
-//             question={question.question}
-//             options={question.options}
-//             correctOption={question.correctAnswer}
-//             handleCorrectAnswer={handleCorrectAnswer}
-//           />
-//         </>
-//       ))}
-//       {correctAnswers === totalQuestions && (
-//         <>
-//           {/* {running && <Confetti run={3000} shape="circle" />} */}
-
-//           <p className="text-success">
-//             You have answered all questions correctly!
-//           </p>
-//           <Link
-//             to={`/certificate/${id}`}
-//             role="button"
-//             className="btn btn-primary mb-3"
-//           >
-//             Get Certificate
-//           </Link>
-//         </>
-//       )}
-//     </>
-//   )}
-// </div>
-//   );
-// };
 
 export default Test;
