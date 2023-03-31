64cc1d20a7d4b4efc57a7f5ba49328cfb585ccd6
Maxim N <mnosov622@gmail.com>
2023-03-30 22:20:52 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ const url = "mongodb+srv://max:LitLab@cluster0.qnyvkxl.mongodb.net";
 //update this line to handle cors issues
 app.use(
   cors({
-    origin: "https://litlab-project.vercel.app",
+    origin: "http://localhost:3000",
   })
 );
 
