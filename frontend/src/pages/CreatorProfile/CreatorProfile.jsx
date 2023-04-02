a3419425be06d4a089570961b580b652da6a3691
Maxim N <mnosov622@gmail.com>
2023-04-01 22:45:29 -0400

    creator profile update

--- frontend/src/pages/CreatorProfile/CreatorProfile.jsx
+++ frontend/src/pages/CreatorProfile/CreatorProfile.jsx
@@ -13,6 +13,17 @@ const CreatorProfile = () => {
   const [description, setDescription] = useState("");
   const alert = useAlert();
 
+  const [imagePreview, setImagePreview] = useState(null);
+
+  const handleImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+
+    setProfileImage(selectedImage);
+    setImagePreview(URL.createObjectURL(selectedImage));
+
+    console.log("image", URL.createObjectURL(selectedImage));
+  };
+
   useEffect(() => {
     fetch(`http://localhost:8000/users/${decoded.id}`)
       .then((response) => response.json())
@@ -76,52 +87,86 @@ const CreatorProfile = () => {
       </h3>
       <div className="profile-container">
         <div className="profile-header">
-          {profileData.profileImage ? (
+          {profileData.profileImage && !imagePreview ? (
             <>
               <img
                 className="profile-header__avatar rounded w-25"
                 src={`http://localhost:8000/images/${profileData.profileImage}`}
                 alt="avatar"
               />
-              <div className="">
-                <p className="text-info">Upload an image</p>
-                <input
-                  type="file"
-                  onChange={(e) => {
-                    setProfileImage(e.target.files[0]);
-                  }}
-                />
-              </div>
+              {!imagePreview && (
+                <div className="">
+                  <p className="text-primary">Upload new image</p>
+                  <input type="file" onChange={(e) => handleImageChange(e)} />
+                </div>
+              )}
+
+              {imagePreview && (
+                <>
+                  <div
+                    className="mb-3 text-center"
+                    style={{ marginTop: "-5%" }}
+                  >
+                    <p className="text-primary fs-1">Preview image</p>
+                    <img
+                      src={imagePreview}
+                      className="image-preview"
+                      alt="preview"
+                    />
+                  </div>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => setImagePreview(null)}
+                  >
+                    Remove
+                  </button>
+                </>
+              )}
             </>
           ) : (
             <>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="106"
-                height="106"
-                fill="currentColor"
-                class="bi bi-person"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
-              </svg>
-              <div className="">
-                <p className="text-primary">Upload an image</p>
-                <div class="input-container d-flex  justify-content-center align-items-center file-input">
-                  <p className="fs-4">
-                    <i className="bi bi-upload fs-1"></i>
-                  </p>
-                  <input
-                    type="file"
-                    onChange={(e) => {
-                      setProfileImage(e.target.files[0]);
-                    }}
-                    required
-                    id="photo"
-                    className="input-file"
-                  />
-                </div>
-              </div>
+              {!imagePreview && (
+                <>
+                  {" "}
+                  <div className="mx-auto text-center">
+                    <div class="input-container d-flex text-center mx-auto  justify-content-center align-items-center file-input">
+                      <p className="fs-4">
+                        <i className="bi bi-upload fs-1"></i>
+                      </p>
+                      <input
+                        type="file"
+                        onChange={(e) => handleImageChange(e)}
+                        required
+                        id="photo"
+                        className="input-file"
+                      />
+                    </div>
+                  </div>
+                </>
+              )}
+
+              {imagePreview && (
+                <>
+                  <div
+                    className="mb-3 text-center"
+                    style={{ marginTop: "-5%" }}
+                  >
+                    <p className="text-primary fs-1">Preview image</p>
+                    <img
+                      src={imagePreview}
+                      className="image-preview"
+                      alt="preview"
+                    />
+                  </div>
+
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => setImagePreview(null)}
+                  >
+                    Remove
+                  </button>
+                </>
+              )}
             </>
           )}
         </div>
