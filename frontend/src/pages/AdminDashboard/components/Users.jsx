9689a7d84495e1980a3c2c5e57e38a1978fc2c6f
Maxim N <mnosov622@gmail.com>
2023-04-20 22:35:24 -0400

    styling update

--- frontend/src/pages/AdminDashboard/components/Users.jsx
+++ frontend/src/pages/AdminDashboard/components/Users.jsx
@@ -236,7 +236,7 @@ const Users = () => {
         <h2 className="text-center mb-5 mt-5">Total Revenue</h2>
         <div
           className="featuredItem text-center w-25 mx-auto"
-          style={{ cursor: "default", width: "auto" }}
+          style={{ cursor: "default", width: "auto !important" }}
         >
           <span className="featuredMoney text-center">
             {/* {loading ? (
